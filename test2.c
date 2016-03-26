eec516c5bd8481a0306d3cd86e31a71df67657f9
chadaram <chadaramsanjay123@gmail.com>
2016-03-26 15:06:39 +0530

    Update test2.c

--- test2.c
+++ test2.c
@@ -1,20 +1,33 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<malloc.h>
 main()
 {
-FILE *f;
-int i;
-char *c,buf;
-c=(char*)malloc(1000*(sizeof(char)));
-f=fopen("3.c","r");
-for(i=0;buf!=EOF;i++)
+int yes=1,no=0,choice,a,b,i,n,v[100][100];
+printf("enter the no. of nodes:");
+scanf("%d",&n);
+for(a=1;a<n;a++)
 {
-        buf=fgetc(f);
-        c[i]=buf+10;
+for(b=1;b<n;b++)
+{
+printf("is there any edge between v-a and v-b");
 }
-fclose(f);
-f=fopen("3.c","w");
-fputs(c,f);
-fclose(f);
+}
+printf("type yes or no");
+scanf("%d",&choice);
+if(choice==yes)
+{
+v[a][b]=1;
+}
+elseif(choice==no)
+{v[a][b]=0;
+}
+printf("the adjacency matrix is:");
+for(a=1;a<n;a++)
+{
+for(b=1;b<n;b++)
+{
+printf("%3d",v[a][b]);
+}
+printf("\n\n");
+}
 }
